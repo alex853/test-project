fa8338f006be4be4f0d59c65613f459196a2717f
alex853 <alexxxey.kornev@gmail.com>
2026-08-12 14:31:02 +0300

    commit to another branch

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -4,13 +4,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello,fsasdefas World!");
+        System.out.println("Hellofsasadfsa, World!");
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+        System.out.println("Hello,sdfasdfs World!");
     }
 }
